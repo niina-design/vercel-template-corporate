4d7f5a10032dc38fc5eeb043298d535a3e6e250a
yangae <cyangae@gmail.com>
2023-09-26 15:59:47 +0900

    カテゴリコンポーネントの中身をいったんコメントアウト

--- components/Category/index.tsx
+++ components/Category/index.tsx
@@ -6,5 +6,7 @@ type Props = {
 };
 
 export default function Category({ category }: Props) {
-  return <span className={styles.tag}>{category.name}</span>;
+  return <span className={styles.tag}>
+    {/* {category.name} */}
+    </span>;
 }
